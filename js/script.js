2f34c95c89c59fabbdd47f9e669507630c844683
Mariam-Kasim <124389357+Mariam-Kasim@users.noreply.github.com>
2023-06-14 03:26:21 +0000

    main

--- js/script.js
+++ js/script.js
@@ -1,5 +1,19 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Mariam Kasim
+// Created on: Mar 2023
 // This file contains the JS functions for index.html
+
+"use strict"
+const getrandomquote = async (URLAdress) => {
+  try {
+    const response = await fetch(URLAdress)
+    const jsonData = await response.json()
+    document.getElementById("quote").innerHTML = jsonData.quote
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+getrandomquote("https://api.kanye.rest/")
+
